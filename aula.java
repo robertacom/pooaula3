0781e20f068eddf5e0c96170c2dc41d770319269
aluno <aluno@ad.ifalcvic.local>
2017-10-03 10:54:51 -0300

    primeira aula

---
+++ aula.java
@@ -0,0 +1,22 @@
+
+
+public class aula {
+
+	public static void main(String[] args){
+
+                // somar dois numero e imprimir o resultado
+
+ 	        int numeroa = 50;
+		int numerob = 5;
+		
+		int soma = numeroa / numerob;
+
+
+
+		System.out.println(soma);
+
+	}
+
+}
+
+
